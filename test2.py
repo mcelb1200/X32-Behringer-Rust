6ce01d134a45b35ef738b586873384a49cffdc55
mcelb1200 <156725804+mcelb1200@users.noreply.github.com>
2026-06-21 20:46:35 +1000

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import shlex
-
-cmd = "cargo test -p x32_usb --test integration_test test_not_connected"
-result = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
-print(f"STDOUT: '{result.stdout}'")
-print(f"STDERR: '{result.stderr}'")
