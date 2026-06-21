d9b606c69cd606a52c1af69eda1b790f67438048
mcelb1200 <156725804+mcelb1200@users.noreply.github.com>
2026-06-21 20:45:31 +1000

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import shlex
-
-cmd = "./target/debug/x32_usb --ip 127.0.0.1:10047 ls"
-result = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
-print(f"STDOUT: '{result.stdout}'")
-print(f"STDERR: '{result.stderr}'")
-print(f"CODE: {result.returncode}")
